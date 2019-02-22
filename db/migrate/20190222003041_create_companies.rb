0c88aafadafbc35dc50553d5da94cdffb3d03292
Joseph Banass <jbanass@gmail.com>
2019-02-22 11:40:44 -0600

    updating companies schema

--- db/migrate/20190222003041_create_companies.rb
+++ db/migrate/20190222003041_create_companies.rb
@@ -1,7 +1,6 @@
 class CreateCompanies < ActiveRecord::Migration[5.2]
   def change
     create_table :companies do |t|
-      t.string :code, null: false
       t.string :name, null: false
       t.string :address1
       t.string :address2
