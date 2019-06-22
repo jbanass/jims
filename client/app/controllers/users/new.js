faeccd2c720dbea86d4630615468a476f43d2800
Joseph Banass <jbanass@gmail.com>
2019-06-21 23:36:05 -0500

    functional users page

--- client/app/controllers/users/new.js
+++ client/app/controllers/users/new.js
@@ -1,4 +1,25 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
 
 export default Controller.extend({
+  actions: {
+    back(model) {
+      this.model.rollbackAttributes();
+      this.transitionToRoute("users");
+    },
+    cancel(model) {
+      this.model.rollbackAttributes();
+      this.transitionToRoute("users");
+    },
+    saveUser(model) {
+      self = this;
+      model
+        .save()
+        .then(function(success) {
+          self.transitionToRoute("users.show", model);
+        })
+        .catch(() => {
+          console.log(model);
+        });
+    }
+  }
 });
